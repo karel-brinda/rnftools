889ee11a13f1d479029372e398f38c7111d0fab6
Karel Břinda <karel.brinda@gmail.com>
2015-02-06 11:16:35 +0100

    Reads count bug correction

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ if sys.version_info < (3,2):
 	exit(1)
 
 setup(
-	name = 'Mishmash',
+	name = 'MIShmash',
 	packages = ['mishmash'],
 	package_dir = {"mishmash" : "mishmash"},
 	package_data = {"mishmash" : ["*.snake"] },
-	version = '0.0.1',
+	version = '0.0.2',
 	description = 'Program for simulating NGS read respecting the RNF read-name format',
 	#long_description = """ \  """,
 	install_requires=[
