f3340782694a4ed66da583bb26c5db9cc4c515b0
Karel Břinda <karel.brinda@gmail.com>
2015-02-23 00:36:03 +0100

    Read suffixed added, CuReSim update, WgSim single-end, many small updates

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
 	packages = ['mishmash'],
 	package_dir = {"mishmash" : "mishmash"},
 	package_data = {"mishmash" : ["*.snake"] },
-	version = '0.0.5',
+	version = '0.0.6',
 	description = 'Program for simulating NGS read respecting the RNF read-name format',
 	#long_description = """ \  """,
 	install_requires=[
