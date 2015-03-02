83f8140d73eb29b3602ea17fcca6d04532a45888
Karel Břinda <karel.brinda@gmail.com>
2015-03-02 15:59:06 +0100

    Test update

--- tests.sh
+++ tests.sh
@@ -1,75 +1,82 @@
 #! /usr/bin/env bash
 
-set -e -o pipefail; 
+echo
+echo
+echo
+echo
+echo
+echo
+echo "===================================================================="
+echo
+echo
+echo
+echo "                 TEST: example 1"
+echo
+echo
+echo
+echo "===================================================================="
+echo
+echo
+echo
 
 (
-	echo
-	echo
-	echo
-	echo
-	echo
-	echo
-	echo "===================================================================="
-	echo
-	echo
-	echo
-	echo "                 TEST: example 1"
-	echo
-	echo
-	echo
-	echo "===================================================================="
-	echo
-	echo
-	echo
+	set -ex -o pipefail; 
+	rm -fR ~/.smbl
 
 	cd examples/01_simple_read_simulation
 	snakemake -p
 )
 
+echo
+echo
+echo
+echo
+echo
+echo
+echo "===================================================================="
+echo
+echo
+echo
+echo "                 TEST: example 2" 
+echo
+echo
+echo
+echo "===================================================================="
+echo
+echo
+echo
+
 (
-	echo
-	echo
-	echo
-	echo
-	echo
-	echo
-	echo "===================================================================="
-	echo
-	echo
-	echo
-	echo "                 TEST: example 2" 
-	echo
-	echo
-	echo
-	echo "===================================================================="
-	echo
-	echo
-	echo
-	
+	set -ex -o pipefail; 
+	rm -fR ~/.smbl
+
 	cd examples/02_more_complex_read_simulation
 	snakemake -p
 )
 
+echo
+echo
+echo
+echo
+echo
+echo
+echo "===================================================================="
+echo
+echo
+echo
+echo "                 TEST: example 3" 
+echo
+echo
+echo
+echo "===================================================================="
+echo
+echo
+echo
+
 (
-	echo
-	echo
-	echo
-	echo
-	echo
-	echo
-	echo "===================================================================="
-	echo
-	echo
-	echo
-	echo "                 TEST: example 3" 
-	echo
-	echo
-	echo
-	echo "===================================================================="
-	echo
-	echo
-	echo
-	
+	set -ex -o pipefail; 
+	rm -fR ~/.smbl
+
 	cd examples/03_mapper_evaluation_SE
 	
 	cd bams
