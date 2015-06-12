774d6242525922a8b378b8bf77cafb590c6932f7
Karel Břinda <karel.brinda@gmail.com>
2015-06-12 21:59:57 +0200

    Tests for Mason SE/PE, Art PE

--- tests/command_line/rnftools_art2rnf.sh
+++ tests/command_line/rnftools_art2rnf.sh
@@ -12,12 +12,24 @@ $ART_ILLUMINA -sam \
 	--in $FA \
 	--len 100 \
 	--fcov 1 \
-	--out art
+	--out art_se \
 
 rnftools art2rnf \
 	--fasta-index ${FA}.fai \
-	--sam art.sam \
+	--sam art_se.sam \
 	--rnf-fastq _art_rnf_se.fq \
 
 # 2) PE test, no contamination
-#rnftools art2rnf --fasta-index ${FA}.fai --fastq reads_in_rnf.fq --wgsim-fastq-1 wgsim_1.fq --wgsim-fastq-2 wgsim_2.fq
+$ART_ILLUMINA -sam \
+	--in $FA \
+	--len 100 \
+	--paired \
+	--mflen 200 \
+	--sdev 10 \
+	--fcov 1 \
+	--out art_pe \
+
+rnftools art2rnf \
+	--fasta-index ${FA}.fai \
+	--sam art_pe.sam \
+	--rnf-fastq _art_rnf_pe.fq \
