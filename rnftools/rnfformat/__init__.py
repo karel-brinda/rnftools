86646bd09127fc118bef5de5dcfe82698ba87f69
Karel Břinda <karel.brinda@gmail.com>
2015-08-14 00:06:19 +0200

    Minifix

--- rnftools/rnfformat/__init__.py
+++ rnftools/rnfformat/__init__.py
@@ -3,9 +3,8 @@
 from .FqCreator import *
 from .FqMerger import *
 from .ReadTuple import *
+from .RnfLifter import *
 from .RnfProfile import *
 from .Segment import *
 from .Validator import *
 
-from .RnfLifter import *
-from .ChainDict import *
