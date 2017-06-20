d79f8750780d376e9c135c0da2c4a685700baa41
Karel Břinda <karel.brinda@gmail.com>
2017-06-20 17:24:54 -0400

    Formatting

--- rnftools/rnfformat/FqCreator.py
+++ rnftools/rnfformat/FqCreator.py
@@ -2,6 +2,7 @@ import snakemake
 import rnftools
 import os
 
+
 class FqCreator:
 	"""Class for writing RNF reads to FASTQ files.
 
@@ -21,25 +22,25 @@ class FqCreator:
 	"""
 
 	def __init__(
-				self,
-				fastq_fo,
-				read_tuple_id_width=16,
-				genome_id_width=2,
-				chr_id_width=2,
-				coor_width=8,
-				info_reads_in_tuple=True,
-				info_simulator=None,
-			):
-		self._info_simulator=info_simulator
-		self._info_reads_in_tuple=info_reads_in_tuple
-		self._rnf_profile=rnftools.rnfformat.RnfProfile(
-				read_tuple_id_width=read_tuple_id_width,
-				genome_id_width=genome_id_width,
-				chr_id_width=chr_id_width,
-				coor_width=coor_width,
-			)
-		self._fq_file=fastq_fo
-		self.current_read_tuple_id=None
+			self,
+			fastq_fo,
+			read_tuple_id_width=16,
+			genome_id_width=2,
+			chr_id_width=2,
+			coor_width=8,
+			info_reads_in_tuple=True,
+			info_simulator=None,
+	):
+		self._info_simulator = info_simulator
+		self._info_reads_in_tuple = info_reads_in_tuple
+		self._rnf_profile = rnftools.rnfformat.RnfProfile(
+			read_tuple_id_width=read_tuple_id_width,
+			genome_id_width=genome_id_width,
+			chr_id_width=chr_id_width,
+			coor_width=coor_width,
+		)
+		self._fq_file = fastq_fo
+		self.current_read_tuple_id = None
 		self.empty()
 
 	def __del__(self):
@@ -49,35 +50,35 @@ class FqCreator:
 		"""Flush the internal buffer of reads.
 		"""
 		if not self.is_empty():
-			suffix_comment_buffer=[]
+			suffix_comment_buffer = []
 			if self._info_simulator is not None:
 				suffix_comment_buffer.append(self._info_simulator)
 			if self._info_reads_in_tuple:
-				#todo: orientation (FF, FR, etc.)
-				#orientation="".join([])
+				# todo: orientation (FF, FR, etc.)
+				# orientation="".join([])
 				suffix_comment_buffer.append("reads-in-tuple:{}".format(len(self.seqs_bases)))
-			if len(suffix_comment_buffer)!=0:
-				suffix_comment="[{}]".format(",".join(suffix_comment_buffer))
+			if len(suffix_comment_buffer) != 0:
+				suffix_comment = "[{}]".format(",".join(suffix_comment_buffer))
 			else:
-				suffix_comment=""
+				suffix_comment = ""
 
-			rnf_name=self._rnf_profile.get_rnf_name(
-						rnftools.rnfformat.ReadTuple(
-								segments=self.segments,
-								read_tuple_id=self.current_read_tuple_id,
-								suffix=suffix_comment,
-							)
-					)
+			rnf_name = self._rnf_profile.get_rnf_name(
+				rnftools.rnfformat.ReadTuple(
+					segments=self.segments,
+					read_tuple_id=self.current_read_tuple_id,
+					suffix=suffix_comment,
+				)
+			)
 			fq_reads = [
 				os.linesep.join([
-						"@{rnf_name}{read_suffix}".format(
-								rnf_name=rnf_name,
-								read_suffix="/{}".format(str(i+1)) if len(self.seqs_bases)>1 else "",
-							),
-						self.seqs_bases[i],
-						"+",
-						self.seqs_qualities[i],
-					])
+					"@{rnf_name}{read_suffix}".format(
+						rnf_name=rnf_name,
+						read_suffix="/{}".format(str(i + 1)) if len(self.seqs_bases) > 1 else "",
+					),
+					self.seqs_bases[i],
+					"+",
+					self.seqs_qualities[i],
+				])
 				for i in range(len(self.seqs_bases))
 			]
 			self._fq_file.write(os.linesep.join(fq_reads))
@@ -87,22 +88,21 @@ class FqCreator:
 	def empty(self):
 		"""Empty all internal buffers.
 		"""
-		self.seqs_bases=[]
-		self.seqs_qualities=[]
-		self.segments=[]
+		self.seqs_bases = []
+		self.seqs_qualities = []
+		self.segments = []
 
 	def is_empty(self):
 		"""All internal buffer empty?
 		"""
-		return self.seqs_bases==[] and self.seqs_qualities==[] and self.segments==[]
-
+		return self.seqs_bases == [] and self.seqs_qualities == [] and self.segments == []
 
 	def add_read(self,
-				read_tuple_id,
-				bases,
-				qualities,
-				segments,
-			):
+			read_tuple_id,
+			bases,
+			qualities,
+			segments,
+	):
 
 		"""Add a new read to the current buffer. If it is a new read tuple (detected from ID), the buffer will be flushed.
 
@@ -117,9 +117,9 @@ class FqCreator:
 		assert type(qualities) is str, "Wrong type of qualities: '{}'".format(qualities)
 		assert type(segments) is tuple or type(segments) is list
 
-		if self.current_read_tuple_id!=read_tuple_id:
+		if self.current_read_tuple_id != read_tuple_id:
 			self.flush_read_tuple()
-		self.current_read_tuple_id=read_tuple_id
+		self.current_read_tuple_id = read_tuple_id
 
 		self.seqs_bases.append(bases)
 		self.seqs_qualities.append(qualities)
--- rnftools/rnfformat/ReadTuple.py
+++ rnftools/rnfformat/ReadTuple.py
@@ -4,6 +4,7 @@ import re
 
 read_tuple_destr_pattern = re.compile(r'(.*)__([0-9abcdef]+)__(\([0-9abcdefFRN,]*\))(,\([0-9abcdefFRN,]*\))*__(.*)')
 
+
 class ReadTuple:
 	"""Class for a RNF read tuple.
 
@@ -15,27 +16,27 @@ class ReadTuple:
 	"""
 
 	def __init__(
-				self,
-				segments=[],
-				read_tuple_id=0,
-				prefix="",
-				suffix="",
-			):
+			self,
+			segments=[],
+			read_tuple_id=0,
+			prefix="",
+			suffix="",
+	):
 
 		assert type(segments) is tuple or type(segments) is list, "Wrong type of segments: '{}'".format(segments)
 		assert type(read_tuple_id) is int, "Wrong type of read_tuple_id: '{}'".format(read_tuple_id)
 		assert type(prefix) is str, "Wrong type of prefix: '{}'".format(prefix)
 		assert type(suffix) is str, "Wrong type of suffix: '{}'".format(suffix)
 
-		self.read_tuple_id=read_tuple_id
-		self.segments=segments
-		self.prefix=prefix
-		self.suffix=suffix
+		self.read_tuple_id = read_tuple_id
+		self.segments = segments
+		self.prefix = prefix
+		self.suffix = suffix
 
 	def stringize(
-				self,
-				rnf_profile=RnfProfile(),
-			):
+			self,
+			rnf_profile=RnfProfile(),
+	):
 		"""Create RNF representation of this read.
 
 		Args:
@@ -46,23 +47,23 @@ class ReadTuple:
 		"""
 
 		sorted_segments = sorted(self.segments,
-								key=lambda x: (
-									x.genome_id*(10**23) + 
-									x.chr_id*(10**21) +
-									(x.left+(int(x.left==0)*x.right-1))*(10**11) +
-									x.right*(10**1) + 
-									int(x.direction=="F")
-								)
-							)
+			key=lambda x: (
+				x.genome_id * (10 ** 23) +
+				x.chr_id * (10 ** 21) +
+				(x.left + (int(x.left == 0) * x.right - 1)) * (10 ** 11) +
+				x.right * (10 ** 1) +
+				int(x.direction == "F")
+			)
+		)
 
-		segments_strings=[
-				x.stringize(rnf_profile) for x in sorted_segments
-			]
+		segments_strings = [
+			x.stringize(rnf_profile) for x in sorted_segments
+		]
 
-		read_tuple_name="__".join(
+		read_tuple_name = "__".join(
 			[
 				self.prefix,
-				format(self.read_tuple_id,'x').zfill(rnf_profile.read_tuple_id_width),
+				format(self.read_tuple_id, 'x').zfill(rnf_profile.read_tuple_id_width),
 				",".join(segments_strings),
 				self.suffix,
 			]
@@ -81,24 +82,25 @@ class ReadTuple:
 			ValueError
 		"""
 
-		#todo: assert -- starting with (, ending with )
-		#(prefix,read_tuple_id,segments_t,suffix)=(text).split("__")
-		#segments=segments_t.split("),(")
-		m=read_tuple_destr_pattern.match(string)
+		# todo: assert -- starting with (, ending with )
+		# (prefix,read_tuple_id,segments_t,suffix)=(text).split("__")
+		# segments=segments_t.split("),(")
+		m = read_tuple_destr_pattern.match(string)
 		if not m:
-			smbl.messages.error("'{}' is not a valid read name with respect to the RNF specification".format(string),program="RNFtools",subprogram="RNF format",exception=ValueError)
-		groups=m.groups()
-		#todo: check number of groups
-		self.prefix=groups[0]
-		read_tuple_id=groups[1]
-		self.read_tuple_id=int(read_tuple_id,16)
-		self.segments=[]
-		segments_str=groups[2:-1]
+			smbl.messages.error("'{}' is not a valid read name with respect to the RNF specification".format(string),
+				program="RNFtools", subprogram="RNF format", exception=ValueError)
+		groups = m.groups()
+		# todo: check number of groups
+		self.prefix = groups[0]
+		read_tuple_id = groups[1]
+		self.read_tuple_id = int(read_tuple_id, 16)
+		self.segments = []
+		segments_str = groups[2:-1]
 		for b_str in segments_str:
 			if b_str is not None:
-				if b_str[0]==",":
-					b_str=b_str[1:]
-				b=rnftools.rnfformat.Segment()
+				if b_str[0] == ",":
+					b_str = b_str[1:]
+				b = rnftools.rnfformat.Segment()
 				b.destringize(b_str)
 				self.segments.append(b)
-		self.suffix=groups[-1]
+		self.suffix = groups[-1]
